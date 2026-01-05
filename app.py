6fb96d15e948d1af8a871cbfdec61a3b61035758
mo3lyana <mulyana.nana@gmail.com>
2026-01-05 10:12:37 +0700

    add jabatan di app.py

--- app.py
+++ app.py
@@ -26,7 +26,6 @@ def info():
     return jsonify({
         'name': 'Flask CI/CD Demo',
         'author': 'Nana UTMJ',
-        'jabatan': 'Dosen',
         'features': ['Auto Build', 'Auto Deploy', 'Docker']
     })
 
